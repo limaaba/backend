a7dca8b78d5228427e1e5a88285f8d4f04a71d10
Ton Nom <ton.email@example.com>
2025-09-05 04:21:17 +0000

    first

---
+++ app/Http/Controllers/Api/FilmController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FilmController extends Controller
+{
+    function index() {
+        return 'Film ';
+    }
+}
