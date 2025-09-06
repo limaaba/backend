9ef0c5765ef8a1867bbd843faecbe3a43d3e3669
Ton Nom <ton.email@example.com>
2025-09-06 07:19:15 +0000

    2

--- app/Http/Controllers/Api/FilmController.php
+++ app/Http/Controllers/Api/FilmController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Film;
 use Illuminate\Http\Request;
 
 class FilmController extends Controller
 {
-    function index() {
-        return 'Film ';
-    }
+    // public function index() {
+    //     return 'Film ';
+    // }
+
+    public function index() {
+    $totalFilms = Film::all();
+    return response()->json([
+        'total' => $totalFilms,
+        'status' => 200
+    ]);
+}
 }
--- routes/api.php
+++ routes/api.php
@@ -3,15 +3,22 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\FilmController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
 */
+
+// Route de test simple
+Route::get('/test-api', function () {
+    return response()->json([
+        'message' => 'API Laravel fonctionne !',
+        'timestamp' => now()
+    ]);
+});
+
 Route::get('/films', [FilmController::class, 'index']);
-// Here is where you can register API routes for your application. These
-// routes are loaded by the RouteServiceProvider within a group which
-// is assigned the "api" middleware group. Enjoy building your API!
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
